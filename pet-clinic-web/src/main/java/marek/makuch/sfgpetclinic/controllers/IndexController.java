e2415b2c3251896be69008cc4b80505d58897ae5
NullTerrier <marek.aureliusz.makuch@gmail.com>
2018-08-16 22:30:38 +0200

    Create Index page and index controller. Closes #8

---
+++ pet-clinic-web/src/main/java/marek/makuch/sfgpetclinic/controllers/IndexController.java
@@ -0,0 +1,18 @@
+package marek.makuch.sfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/*
+ * @author Marecki
+ */
+
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"", "/", "index.html"})
+    public String index() {
+        return "index";
+    }
+}
