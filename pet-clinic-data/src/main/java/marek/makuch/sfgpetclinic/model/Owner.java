e4f673d557dbe83ea97fd34449e36251d9415ad1
NullTerrier <marek.aureliusz.makuch@gmail.com>
2018-09-30 15:01:59 +0200

    Added visit, pet, petType. Closes #26

--- pet-clinic-data/src/main/java/marek/makuch/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/marek/makuch/sfgpetclinic/model/Owner.java
@@ -3,5 +3,17 @@ package marek.makuch.sfgpetclinic.model;
  * @author Marecki
  */
 
+import java.util.Set;
+
 public class Owner extends Person {
+
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
