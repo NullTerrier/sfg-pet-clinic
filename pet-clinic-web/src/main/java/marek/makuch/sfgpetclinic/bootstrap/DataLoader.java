4537054d94854742d51d067101492a23da619f7f
NullTerrier <marek.aureliusz.makuch@gmail.com>
2018-09-30 15:25:54 +0200

    Bootstraping data for PetTypes. Closes #32

--- pet-clinic-web/src/main/java/marek/makuch/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/marek/makuch/sfgpetclinic/bootstrap/DataLoader.java
@@ -4,7 +4,9 @@ package marek.makuch.sfgpetclinic.bootstrap;
  */
 
 import marek.makuch.sfgpetclinic.model.Owner;
+import marek.makuch.sfgpetclinic.model.PetType;
 import marek.makuch.sfgpetclinic.model.Vet;
+import marek.makuch.sfgpetclinic.services.PetTypeService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import marek.makuch.sfgpetclinic.services.OwnerService;
@@ -17,15 +19,26 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
 
-    public DataLoader(OwnerService ownerMapService, VetService vetMapService) {
+    public DataLoader(OwnerService ownerMapService, VetService vetMapService, PetTypeService petTypeService) {
         this.ownerService = ownerMapService;
         this.vetService = vetMapService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType cat = new PetType();
+        cat.setName("Cat");
+
+        PetType savedDogType = petTypeService.save(dog);
+        PetType savedCatType = petTypeService.save(cat);
+        System.out.println("Loaded pet Types");
 
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
