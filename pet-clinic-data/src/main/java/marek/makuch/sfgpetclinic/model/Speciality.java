77d6bb06319386fdde7904478dd589a16fc4b138
NullTerrier <marek.aureliusz.makuch@gmail.com>
2018-09-30 15:07:33 +0200

    Added specialities. Closes #27

---
+++ pet-clinic-data/src/main/java/marek/makuch/sfgpetclinic/model/Speciality.java
@@ -0,0 +1,17 @@
+package marek.makuch.sfgpetclinic.model;
+/*
+ * @author Marecki
+ */
+
+public class Speciality extends BaseEntity {
+
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
--- pet-clinic-data/src/main/java/marek/makuch/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/marek/makuch/sfgpetclinic/model/Vet.java
@@ -3,5 +3,17 @@ package marek.makuch.sfgpetclinic.model;
  * @author Marecki
  */
 
+import java.util.Set;
+
 public class Vet extends Person  {
+
+    private Set<Speciality> specialities;
+
+    public Set<Speciality> getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<Speciality> specialities) {
+        this.specialities = specialities;
+    }
 }
