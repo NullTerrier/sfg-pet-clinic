106160c4dfccb81c990a52c1958af91266824ee2
NullTerrier <marek.aureliusz.makuch@gmail.com>
2018-11-20 23:10:49 +0100

    Implement base entity, closes #35

--- pet-clinic-data/src/main/java/marek/makuch/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/marek/makuch/sfgpetclinic/model/BaseEntity.java
@@ -3,10 +3,17 @@ package marek.makuch.sfgpetclinic.model;
  * @author Marecki
  */
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+@MappedSuperclass //establishes this as base class of JPA
 public class BaseEntity implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
